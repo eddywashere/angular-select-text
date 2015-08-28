45dfdd453c91ededad9c412dc513d849fdba5f72
Alex Fraser <alex@phatcore.com>
2015-08-28 23:52:43 +1000

    Allow selection of text in a descendant element
    The target of a selection event can be designated by adding the
    'select-text-target' directive to a descendant. The contents of
    that element will be selected instead of the element that was
    clicked on.
    
    Removed dependency on jQuery.
    
    Using a linked directive and a shared service instead of using
    jQuery's find function to get the selection target element.

--- src/angular-select-text.js
+++ src/angular-select-text.js
@@ -3,27 +3,48 @@
  * angular-select-text directive
  */
 angular.module('angular-select-text', []).
-  directive('selectText', ['$window', function ($window) {
-    var selectElement;
-
+  service('selectElement', ['$window', function($window) {
     if ($window.document.selection) {
-      selectElement = function(element) {
+      return function(element) {
         var range = $window.document.body.createTextRange();
         range.moveToElementText(element[0]);
         range.select();
       };
     } else if ($window.getSelection) {
-      selectElement = function(element) {
+      return function(element) {
         var range = $window.document.createRange();
         range.selectNode(element[0]);
+        $window.getSelection().removeAllRanges();
         $window.getSelection().addRange(range);
       };
     }
+  }]).
 
+  directive('selectText', ['selectElement', function (selectElement) {
     return {
       restrict: 'A',
+      controller: ['$scope', function($scope) {
+        this.register = function(callback) {
+          $scope.callback = callback;
+        };
+      }],
       link: function(scope, element, attrs){
         element.bind('click', function(){
+          if (scope.callback)
+            scope.callback();
+          else
+            selectElement(element);
+        });
+      }
+    };
+  }]).
+
+  directive('selectTextTarget', ['selectElement', function(selectElement) {
+    return {
+      restrict: 'A',
+      require: '^selectText',
+      link: function(scope, element, attrs, selectText) {
+        selectText.register(function() {
           selectElement(element);
         });
       }
